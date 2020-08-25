24fab31d5ae51637dcb53ebbbc32a9813509ec15
Chandima Ranasinghe <chandimapr@gmail.com>
2020-08-25 16:48:54 +0530

    Updates

---
+++ precache-manifest.bb1732fc3ddc38dd3b44e0da3e345891.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a81c21e13d83213981e2209e34152108",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ce257bbcf81b9433b9b",
+    "url": "/static/css/main.47e372c2.chunk.css"
+  },
+  {
+    "revision": "9a328240293188a5c658",
+    "url": "/static/js/2.eb99faf2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eb99faf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce257bbcf81b9433b9b",
+    "url": "/static/js/main.755ad95b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
